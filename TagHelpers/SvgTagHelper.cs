b7724c6d8f93c658c42527bc1188f64e27c9559c
Emilio <emilio.angu.2003@gmail.com>
2026-01-18 22:01:48 +0100

    feat: implementar Library Manager (LibMan)

---
+++ TagHelpers/SvgTagHelper.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace NCS.Prueba.TagHelpers;
+
+[HtmlTargetElement("inline-svg")]
+public class SvgTagHelper : TagHelper
+{
+    private readonly IWebHostEnvironment _env;
+
+    public SvgTagHelper(IWebHostEnvironment env)
+    {
+        _env = env;
+    }
+
+    [HtmlAttributeName("src")]
+    public string Src { get; set; }
+
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        if (string.IsNullOrEmpty(Src)) return;
+
+        var filePath = Path.Combine(_env.WebRootPath, "assets", "svg", Src);
+
+        if (File.Exists(filePath))
+        {
+            var content = File.ReadAllText(filePath);
+            output.TagName = null;
+            output.Content.SetHtmlContent(content);
+        }
+    }
+}
